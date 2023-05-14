ce2152bb36c2dac40136a3c0bde5f655f9809d2e
Luka Robajac <luka.robajac@gmail.com>
2023-05-14 16:25:17 +0200

    initial commit

---
+++ src/main/java/com/example/lr20190024/Lr20190024Application.java
@@ -0,0 +1,13 @@
+package com.example.lr20190024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lr20190024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lr20190024Application.class, args);
+	}
+
+}
