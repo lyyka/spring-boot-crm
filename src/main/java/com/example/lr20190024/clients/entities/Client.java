b8621ae29618aea29e30fa1447bf9cd8c7b9c963
Luka Robajac <luka.robajac@gmail.com>
2023-05-15 00:07:48 +0200

    switch id property to identity. update application.properties.

--- src/main/java/com/example/lr20190024/clients/entities/Client.java
+++ src/main/java/com/example/lr20190024/clients/entities/Client.java
@@ -14,7 +14,7 @@ import java.util.Objects;
 @AllArgsConstructor
 public class Client {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String firstName;
