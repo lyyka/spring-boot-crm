e7a365d7ee6e8cc86b9f59a6a35b76c34551dc94
Luka Robajac <luka.robajac@gmail.com>
2023-05-28 13:33:23 +0200

    fix Like filter concat

--- src/main/java/com/example/lr20190024/common/filters/operations/Like.java
+++ src/main/java/com/example/lr20190024/common/filters/operations/Like.java
@@ -14,7 +14,7 @@ public class Like<T> implements FilterOperation<T> {
     public Predicate toPredicate(Root<T> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
         Expression<String> column = cb.lower(root.get(fields[0]));
         for (int i = 1; i < fields.length; i++) {
-            column = cb.concat(column, cb.lower(root.get(fields[i])));
+            column = cb.concat(cb.concat(column, cb.literal(" ")), cb.lower(root.get(fields[i])));
         }
         return cb.like(column, "%" + value.toLowerCase() + "%");
     }
